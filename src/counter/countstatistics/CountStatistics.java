dcd1dffe2a857c87415752beb9d5f11995b16e38
xyzhu <xyzhu@xyzhu-3-2.(none)>
2011-10-27 16:43:51 +0800

    Delete an screen print out.

--- src/counter/countstatistics/CountStatistics.java
+++ src/counter/countstatistics/CountStatistics.java
@@ -353,7 +353,6 @@ public class CountStatistics {
 		}
 		if(numOperator==0){
 			if(isConstAssign){
-				System.out.println("@@@@@@@@");
 				currentFile.numConstAssign += numassign;
 			}
 			else{
